4f059c47bb803f1ee3aafffcdd8a1a71b7eb9691
Louie D <louie.delrosario97@gmail.com>
2026-06-12 13:02:16 -0700

    Converted Product Model into JPA Entity

--- src/main/java/com/pluralsight/models/Product.java
+++ src/main/java/com/pluralsight/models/Product.java
@@ -1,22 +1,52 @@
 package com.pluralsight.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.context.annotation.EnableMBeanExport;
 
-@Entity
-@Table(name ="products]")
+@Entity                  // '@Entity' tells JPA this class maps to a database
+@Table(name ="products") // '@Table'  tells JPA the specific table we are mapping to
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Product
 {
-    @Id
+    @Id // '@Id' assigns the primary key column ('productId')
+    @Column(name="ProductID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer productId;
+
+    @Column(name = "ProductName")
+    private String productName;
+
+    @Column(name = "SupplierID")
+    private Integer supplierId;
+
+    @Column(name = "CategoryID")
+    private Integer categoryId;
+
+    @Column(name = "QuantityPerUnit")
+    private Integer quantityPerUnit;
+
+    @Column(name = "UnitPrice")
+    private Double unitPrice;
+
+    @Column(name = "UnitsInStock")
+    private Integer unitsInStock;
+
+    @Column(name = "UnitsOnOrder")
+    private Integer unitsOnOrder;
+
+    @Column(name = "ReorderLevel")
+    private Integer reorderLevel;
+
+    @Column(name = "Discontinued")
+    private Integer discontinued;
+
 }
 
 
