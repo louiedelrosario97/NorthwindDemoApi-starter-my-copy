3bb4289f35ebcd821c785c609c7604831dc917b2
Louie D <louie.delrosario97@gmail.com>
2026-06-12 14:23:30 -0700

    Created ProductController

--- src/main/java/com/pluralsight/controllers/exceptions/ProductsController.java
+++ src/main/java/com/pluralsight/controllers/exceptions/ProductsController.java
@@ -1,5 +1,70 @@
 package com.pluralsight.controllers.exceptions;
 
-public class ProductsController {
+import com.pluralsight.models.Product;
+import com.pluralsight.services.ProductsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/products")
+@CrossOrigin
+public class ProductsController
+{
+    private ProductsService productsService;
+
+    @Autowired
+    public ProductsController(ProductsService productsService) { this.productsService = productsService; }
+
+    @GetMapping("")
+    public ResponseEntity<List<Product>> getAll()
+    {
+        var products = productsService.findAllProducts();
+
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Product> getById(@PathVariable int id)
+    {
+        var product = productsService.findProductyById(id);
+
+        return ResponseEntity.ok(product);
+    }
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Product> addProduct(@RequestBody Product product)
+    {
+        var newProduct = productsService.addProduct(product);
+
+        URI location = URI.create("/products/" + product.getProductId());
+
+        return ResponseEntity.created(location).body(newProduct);
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Void> update(@PathVariable int id, @RequestBody Product product)
+    {
+        productsService.updateProduct(id, product);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Void> delete(@PathVariable int id)
+    {
+        productsService.deleteProduct(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
+
 
